34ff3fd0fde762fe29e2b4630a3a53a61b3fb24d
Alfredo Mapelli <alfredo.mapelli@geo-observations.com>
2022-03-16 18:11:49 +0000

    Initial Commit

---
+++ MonitorRGBController/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows.Devices.Enumeration;
+using MonitorRGBController.Models;
+
+namespace MonitorRGBController {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		private ObservableCollection<BLEDevice> KnownDevices = new ObservableCollection<BLEDevice>();
+		private List<DeviceInformation> UnknownDevices = new List<DeviceInformation>();
+
+		private DeviceWatcher deviceWatcher;
+		
+		
+		public MainWindow() {
+			InitializeComponent();
+		}
+		private void ListBLEDvices_Click(object sender, RoutedEventArgs e) {
+			if (this.deviceWatcher == null) {
+				StartBleDeviceWatcher();
+				btnAdd.Content = "Stop enumerating";
+			} else {
+				StopBleDeviceWatcher();
+				btnAdd.Content = "Start enumerating";
+			}
+		}
+	}
+}
---
+++ MonitorRGBController/Models/BLEDevice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Media.Imaging;
+using Windows.Devices.Enumeration;
+
+namespace MonitorRGBController.Models; 
+
+public class BLEDevice {
+	public DeviceInformation DeviceInformation { get; private set; }
+
+	public string Id => DeviceInformation.Id;
+	public string Name => DeviceInformation.Name;
+	public bool IsPaired => DeviceInformation.Pairing.IsPaired;
+	public bool IsConnected => (bool?)DeviceInformation.Properties["System.Devices.Aep.IsConnected"] == true;
+	public bool IsConnectable => (bool?)DeviceInformation.Properties["System.Devices.Aep.Bluetooth.Le.IsConnectable"] == true;
+
+	public IReadOnlyDictionary<string, object> Properties => DeviceInformation.Properties;
+	
+	public event PropertyChangedEventHandler PropertyChanged;
+
+	public void Update(DeviceInformationUpdate deviceInfoUpdate)
+	{
+		DeviceInformation.Update(deviceInfoUpdate);
+
+		OnPropertyChanged("Id");
+		OnPropertyChanged("Name");
+		OnPropertyChanged("DeviceInformation");
+		OnPropertyChanged("IsPaired");
+		OnPropertyChanged("IsConnected");
+		OnPropertyChanged("Properties");
+		OnPropertyChanged("IsConnectable");
+	}
+
+	protected void OnPropertyChanged(string name)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+	}
+}
